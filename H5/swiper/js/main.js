cec6589c3f62dc7b60bbcee44b834762af47eff3
ashayx <973946691@qq.com>
2017-08-10 12:00:34 +0800

    移动端点击测试

--- H5/swiper/js/main.js
+++ H5/swiper/js/main.js
@@ -24,25 +24,25 @@ var right = e(".right-door")
 var door = e(".door")
 var page = e(".page1")
 
-door.onclick = function () {
-    setTimeout(function () {
-        swiper.slideTo(1, 0,false)
-    },2000 )
-    left.style.animation = "moveLeft 2s infinite"
-    right.style.animation = "moveRight 2s infinite"
-    page.style.transition = "all 2s"
-    page.style.transform = "scale(1.8)"
+// door.onclick = function () {
+//     setTimeout(function () {
+//         swiper.slideTo(1, 0,false)
+//     },2000 )
+//     left.style.animation = "moveLeft 2s infinite"
+//     right.style.animation = "moveRight 2s infinite"
+//     page.style.transition = "all 2s"
+//     page.style.transform = "scale(1.8)"
 
-    alert('open')
-}
-door.addEventListener('touchstart',function () {
-    setTimeout(function () {
-        swiper.slideTo(1, 0,false)
-    },2000 )
-    left.style.animation = "moveLeft 2s infinite"
-    right.style.animation = "moveRight 2s infinite"
-    page.style.transition = "all 2s"
-    page.style.transform = "scale(1.8)"
+//     alert('open')
+// }
+// door.addEventListener('touchstart',function () {
+//     setTimeout(function () {
+//         swiper.slideTo(1, 0,false)
+//     },2000 )
+//     left.style.animation = "moveLeft 2s infinite"
+//     right.style.animation = "moveRight 2s infinite"
+//     page.style.transition = "all 2s"
+//     page.style.transform = "scale(1.8)"
 
-    alert('open')
-})
+//     alert('open')
+// })
