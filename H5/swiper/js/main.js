d5839df8f3ffd91f37ae2601901991cc5ff55329
ashayx <973946691@qq.com>
2017-08-10 13:50:11 +0800

    移动端点击测试

--- H5/swiper/js/main.js
+++ H5/swiper/js/main.js
@@ -1,7 +1,5 @@
 var e = sel =>document.querySelector(sel)
-// var music=document.getElementById("music")
 var music = e("#music")
-// var aud=document.getElementById("aud")
 var aud = e("#aud")
 
 music.onclick=function(){
@@ -16,33 +14,33 @@ music.onclick=function(){
         this.style.backgroundSize =  "100% 100%"
         this.style.animation= "null"
     }
-    alert('play')
+    // alert('play')
 }
 
-var left = e(".left-door")
+var left = e(".left-door")  
 var right = e(".right-door")
 var door = e(".door")
 var page = e(".page1")
 
-// door.onclick = function () {
-//     setTimeout(function () {
-//         swiper.slideTo(1, 0,false)
-//     },2000 )
-//     left.style.animation = "moveLeft 2s infinite"
-//     right.style.animation = "moveRight 2s infinite"
-//     page.style.transition = "all 2s"
-//     page.style.transform = "scale(1.8)"
+door.onclick = function () {
+    setTimeout(function () {
+        swiper.slideTo(1, 0,false)
+    },2000 )
+    left.style.animation = "moveLeft 2s infinite"
+    right.style.animation = "moveRight 2s infinite"
+    page.style.transition = "all 2s"
+    page.style.transform = "scale(1.8)"
 
-//     alert('open')
-// }
-// door.addEventListener('touchstart',function () {
-//     setTimeout(function () {
-//         swiper.slideTo(1, 0,false)
-//     },2000 )
-//     left.style.animation = "moveLeft 2s infinite"
-//     right.style.animation = "moveRight 2s infinite"
-//     page.style.transition = "all 2s"
-//     page.style.transform = "scale(1.8)"
+    // alert('open')
+}
+door.addEventListener('touchstart',function () {
+    setTimeout(function () {
+        swiper.slideTo(1, 0,false)
+    },2000 )
+    left.style.animation = "moveLeft 2s infinite"
+    right.style.animation = "moveRight 2s infinite"
+    page.style.transition = "all 2s"
+    page.style.transform = "scale(1.8)"
 
-//     alert('open')
-// })
+    // alert('open')
+})
